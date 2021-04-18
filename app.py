3d1cfde5da7f16db95bda45a7f191fe8a57f5a76
riyad mac2 <riyad@kalke.co>
2021-04-18 14:41:47 +0600

    added login

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import json
 import utils
+import bcrypt
+import datetime
+from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, request, jsonify
-from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 
@@ -21,14 +23,31 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+    last_login = db.Column(db.DateTime, default=datetime.datetime.now())
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+    @classmethod
+    def get_or_404_via_username(cls, params):
+        user = User.query.filter_by(username=params["username"]).first()
+        if not user:
+            return None
+        return user
 
     def save(self):
         db.session.add(self)
         db.session.commit()
         return
+
+    def hash_password(self, password):
+        password = bcrypt.hashpw(password.encode(
+            "utf-8"), bcrypt.gensalt())
+        self.password = password.decode("utf-8")
+        return True
+
+    def verify_password(self, password):
+        return bcrypt.checkpw(password.encode("utf-8"), self.password.encode("utf-8"))
 
     def to_json(self):
         data = {
@@ -38,23 +57,32 @@ class User(db.Model):
         return data
 
 
-@app.route("/", methods=["POST"])
-def create():
+@app.route("/signup", methods=["POST"])
+def signup():
     data = json.loads(request.data)
     obj = User(username=data.get("username"), email=data.get(
-        "email"), password=data.get("password"))
+        "email"))
+    obj.hash_password(data.get("password"))
     obj.save()
     return obj.to_json(), 201
 
 
-@app.route("/", methods=["GET"])
-def list():
-    data = User.query.all()
-    return jsonify(utils.to_json_list(data)), 200
+@app.route("/login", methods=["POST"])
+def login():
+    data = json.loads(request.data)
+    obj = User.get_or_404_via_username(data)
+    if not obj:
+        return {"status": "failed", "msg": "Account with that username not found"}, 401
+
+    verify = obj.verify_password(data["password"])
+    if not verify:
+        return {"status": "failed", "msg": "Password doesn't match"}, 401
+
+    return {"status": "success", "msg": "Jwt generated", "data": utils.jwt_creator(utils.generate_user_payload(obj))}, 200
 
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
 
-if __name__ == '__main__':
-    manager.run()
+# if __name__ == '__main__':
+#     manager.run()
--- utils.py
+++ utils.py
@@ -1,11 +1,33 @@
+import os
+import jwt
+import datetime
+from dotenv import load_dotenv
+load_dotenv()
 
 
 def to_json_list(list_obj):
     arr = []
     for i in list_obj:
-        # arr.append({
-        #     "id": i.id,
-        #     "username": i.email
-        # })
         arr.append(i.to_json())
     return arr
+
+
+def generate_user_payload(obj):
+    data = {
+        "id": obj.id,
+        "username": obj.username,
+        "email": obj.email,
+        "last_login": str(obj.last_login)
+    }
+    obj.last_login = datetime.datetime.now()
+    obj.save()
+    return data
+
+
+def jwt_creator(payload):
+    return jwt.encode(payload, os.getenv(
+        "JWT_SECRET"), algorithm="HS256")
+
+
+def jwt_verifier(jwt_token):
+    return jwt.decode(jwt_token, os.getenv("JWT_SECRET"), algorithms=["HS256"])
